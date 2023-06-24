2a09dd31d76d993e193f69225eb44c032f84b8c9
Mig999 <88632519+Mig999@users.noreply.github.com>
2023-06-24 16:04:19 +0200

    With eyes

--- Snake ML Unity project/Assets/Scripts/AgentControler.cs
+++ Snake ML Unity project/Assets/Scripts/AgentControler.cs
@@ -11,27 +11,14 @@ public class AgentControler : Agent
     [SerializeField] private Transform targetTransform;
     [SerializeField] GameObject player;
     Snake snake;
-    BufferSensorComponent bufferSensorComponent;
 
-    public int maxSegmentsNumber=989;
-    //private float[] segmentsPosition;
-    //public bool[][][] map;
-    //public int mapLenght = 45;
-    //public int mapHeight = 25;
-    //private Vector3 foodPosition;
-    //private Vector3[] snakePositions;
-
-    //private string textToConsole;
+    private float distanceToObject;
     
 
     protected override void Awake()
     {
         base.Awake();
         snake = player.GetComponent<Snake>();
-        //bufferSensorComponent = player.GetComponent<BufferSensorComponent>();
-
-        //segmentsPosition = new float[2];
-        //CreateMap();
 
     }
     
@@ -52,22 +39,10 @@ public class AgentControler : Agent
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(targetTransform.localPosition);
 
-        for(int i =1; i<maxSegmentsNumber+1; i++){
-            if(i<snake.segments.Count){
-                sensor.AddObservation(snake.segments[i].position);
-            }else{
-                sensor.AddObservation(Vector3.zero);
-            }
-        }
-
-        /*
-        for(int i =1; i<snake.segments.Count; i++){
-            segmentsPosition[0] = snake.segments[i].position.x;
-            segmentsPosition[1] = snake.segments[i].position.y;
-            bufferSensorComponent.AppendObservation(segmentsPosition);
-        }
-        */
-        
+        sensor.AddObservation(DistanceToObject(Vector2.up));
+        sensor.AddObservation(DistanceToObject(Vector2.right));
+        sensor.AddObservation(DistanceToObject(Vector2.down));
+        sensor.AddObservation(DistanceToObject(Vector2.left));        
     }
     /*
     
@@ -150,114 +125,26 @@ public class AgentControler : Agent
         EndEpisode();
     }
     
-    /*
-    private void CreateMap(){
-        map = new bool[mapLenght][][];
-        for(int i= 0; i< mapLenght; i++){
-            map[i] = new bool[mapHeight][];
-            for(int x=0; x<mapHeight; x++){
-                map[i][x] = new bool[4];
+    
+    private float DistanceToObject(Vector2 directionRay)
+    {
+        Debug.Log("Funcionand");
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionRay);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Funcionando2");
+            if (hit.collider.CompareTag("Obstacle"))
+            {
+                float distanceToObject = hit.distance; // Get the distance to the hit object
+                Debug.Log("Distance to obstacle: " + distanceToObject);
             }
         }
 
-        for(int i= 0; i< mapLenght; i++){
-            for(int x=0; x<mapHeight; x++){
-                map[i][x][0] = false; // Wall
-                map[i][x][1] = false; // Food
-                map[i][x][2] = false; // Snake Segments
-                map[i][x][3] = false; // Head of Snake
-                if(i==0 || x==0 || i==mapLenght-1 || x==mapHeight-1){
-                    map[i][x][0] = true;
-                }
-            }
-        }
+        return distanceToObject;
 
     }
-    */
-    /*
-    private void UpdateMap(){
-        for(int i= 0; i< mapLenght; i++){
-            for(int x=0; x<mapHeight; x++){
-                map[i][x][1] = false;
-                map[i][x][2] = false;
-            }
-        }
-
-        foodPosition = targetTransform.position;
-        map[(int)Mathf.Round(foodPosition.x)+(mapLenght+1)/2][(int)Mathf.Round(foodPosition.y)+(mapHeight+1)/2][1] = true;
-
-        snakePositions = new Vector3[snake.segments.Count];
-        snakePositions[0] = snake.transform.position;
-        for(int i= 1; i< snake.segments.Count; i++){
-            snakePositions[i] = snake.segments[i].position;
-        }
-        for(int i=0; i<snakePositions.Length; i++){
-            map[(int)Mathf.Round(snakePositions[i].x)+(mapLenght+1)/2][(int)Mathf.Round(snakePositions[i].y)+(mapHeight+1)/2][2] = true;
-        }    
-    }
-    */
-    /*
-    private void Update(){
-        if(Input.GetKeyDown(KeyCode.T)){
-            ShowMap();
-        }
-    }
-
-    private void ShowMap(){
-        textToConsole = "";
-        Debug.Log("Wall");
-        for(int x=0; x<mapHeight; x++){
-            for(int i= 0; i< mapLenght; i++){
-                if(map[i][x][0]==true){
-                    textToConsole += "1";
-                } else{
-                    textToConsole += "0";
-                }
-            }
-            Debug.Log(textToConsole);
-            textToConsole = "";
-        }
-        textToConsole = "";
-        Debug.Log("Food");
-        for(int x=0; x<mapHeight; x++){
-            for(int i= 0; i< mapLenght; i++){
-                if(map[i][x][1]==true){
-                    textToConsole += "1";
-                } else{
-                    textToConsole += "0";
-                }
-            }
-            Debug.Log(textToConsole);
-            textToConsole = "";
-        }
-        textToConsole = "";
-        Debug.Log("Segments");
-        for(int x=0; x<mapHeight; x++){
-            for(int i= 0; i< mapLenght; i++){
-                if(map[i][x][2]==true){
-                    textToConsole += "1";
-                } else{
-                    textToConsole += "0";
-                }
-            }
-            Debug.Log(textToConsole);
-            textToConsole = "";
-        }
-        textToConsole = "";
-        Debug.Log("Head");
-        for(int x=0; x<mapHeight; x++){
-            for(int i= 0; i< mapLenght; i++){
-                if(map[i][x][3]==true){
-                    textToConsole += "1";
-                } else{
-                    textToConsole += "0";
-                }
-            }
-            Debug.Log(textToConsole);
-            textToConsole = "";
-        }
-        textToConsole = "";
-    }
-    */
+    
     
 }
